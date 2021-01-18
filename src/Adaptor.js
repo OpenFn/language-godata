f09387838bbcae1758228e4eb62426c585c69a46
mamadou <mamadou.lakhassane@gmail.com>
2021-01-18 13:22:53 +0000

    Add 3 new helpers functions for reference data

--- src/Adaptor.js
+++ src/Adaptor.js
@@ -718,6 +718,166 @@ export function upsertLocation(externalId, goDataLocation, callback) {
   };
 }
 
+/**
+ * Fetch the list of reference data
+ * @public
+ * @example
+ *  listReferenceData(state => {
+ *    console.log(state.data);
+ *    return state;
+ *  });
+ * @function
+ * @param {function} callback - (Optional) Callback function
+ * @returns {Operation}
+ */
+export function listReferenceData(callback) {
+  return state => {
+    const { apiUrl, access_token } = state.configuration;
+
+    return axios({
+      method: 'GET',
+      baseURL: apiUrl,
+      url: '/reference-data',
+      params: {
+        access_token,
+      },
+    })
+      .then(response => {
+        const nextState = composeNextState(state, response.data);
+        if (callback) return callback(nextState);
+        return nextState;
+      })
+      .catch(error => {
+        return error;
+      });
+  };
+}
+
+/**
+ * Get one or multiple reference data from a query filter
+ * @public
+ * @example
+ *  getReferenceData({"where":{"categoryId": "LNG_REFERENCE_DATA_CATEGORY_CENTRE_NAME"}}, state => {
+ *    console.log(state.data);
+ *    return state;
+ *  });
+ * @function
+ * @param {object} query - An object with a query filter parameter
+ * @param {function} callback - (Optional) Callback function
+ * @returns {Operation}
+ */
+export function getReferenceData(query, callback) {
+  return state => {
+    const { apiUrl, access_token } = state.configuration;
+
+    const filter = JSON.stringify(query);
+
+    return axios({
+      method: 'GET',
+      baseURL: apiUrl,
+      url: '/reference-data',
+      params: {
+        filter,
+        access_token,
+      },
+    })
+      .then(response => {
+        const nextState = composeNextState(state, response.data);
+        if (callback) return callback(nextState);
+        return nextState;
+      })
+      .catch(error => {
+        return error;
+      });
+  };
+}
+
+/**
+ * Upsert reference data to godata
+ * @public
+ * @example
+ *  upsertReferenceData('id', {...})
+ * @function
+ * @param {string} externalId - External Id to match
+ * @param {object} goDataReferenceData - an object with some reference data.
+ * @param {function} callback - (Optional) Callback function
+ * @returns {Operation}
+ */
+export function upsertReferenceData(externalId, goDataReferenceData, callback) {
+  return state => {
+    const { apiUrl, access_token } = state.configuration;
+
+    const data = expandReferences(goDataReferenceData)(state);
+
+    const query = { where: {} };
+    query.where[externalId] = data[externalId];
+
+    const filter = JSON.stringify(query);
+
+    return axios({
+      method: 'GET',
+      baseURL: apiUrl,
+      url: '/reference-data',
+      params: {
+        filter,
+        access_token,
+      },
+    })
+      .then(response => {
+        if (response.data.length > 1) {
+          console.log(
+            response.data.length,
+            'reference data found; aborting upsert.'
+          );
+          return response;
+        } else if (response.data.length === 1) {
+          console.log('Reference data found. Performing update.');
+          const referenceDataId = response.data[0].id;
+          console.log('referenceDataId', referenceDataId);
+          return axios({
+            method: 'PUT',
+            baseURL: apiUrl,
+            url: `/reference-data/${referenceDataId}`,
+            params: {
+              access_token,
+            },
+            data,
+          })
+            .then(response => {
+              const nextState = composeNextState(state, response.data);
+              if (callback) return callback(nextState);
+              return nextState;
+            })
+            .catch(error => {
+              return error;
+            });
+        } else {
+          console.log('No reference data found. Performing create.');
+          return axios({
+            method: 'POST',
+            baseURL: apiUrl,
+            url: '/reference-data',
+            params: {
+              access_token,
+            },
+            data,
+          })
+            .then(response => {
+              const nextState = composeNextState(state, response.data);
+              if (callback) return callback(nextState);
+              return nextState;
+            })
+            .catch(error => {
+              return error;
+            });
+        }
+      })
+      .catch(error => {
+        return error;
+      });
+  };
+}
+
 // Note that we expose the entire axios package to the user here.
 exports.axios = axios;
 
